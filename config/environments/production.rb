f93722b2854f27571a4715c82ff18bbdf0404987
Agathe Loubert <agaloubert@hotmail.com>
2018-02-13 10:26:26 +0100

    en cours

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
    config.webpacker.check_yarn_integrity = false  # Settings specified here will take precedence over those in config/application.rb.
 
+  # contact form
   config.action_mailer.default_url_options = { host: 'https://your app.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'contacts/new'
+
   get 'contact/new'
 
   root to: 'pages#home'
   get 'blog', to: 'blog#home', as: :blog
 
-  # resources :contacts, only: [:new, :create]
+  resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
