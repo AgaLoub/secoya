2bc597e06a34a9e1134de725b294b933d157bce3
Agathe Loubert <agaloubert@hotmail.com>
2018-02-26 14:07:05 +0100

    changes

--- app/javascript/components/scroll.js
+++ app/javascript/components/scroll.js
@@ -10,16 +10,16 @@ const links = document.querySelectorAll('.navbar-wagon-link');
 
 function handleClick(event) {
   if (event.target.classList.contains('projet')) {
-    document.querySelector('#projet').scrollIntoView({ behavior: "smooth", block: "start"});
+    document.querySelector('#projet').scrollIntoView({ behavior: "smooth"});
   } else if (event.target.classList.contains('service')) {
-    document.querySelector('#service').scrollIntoView({ behavior: "smooth", block: "start"});
+    document.querySelector('#service').scrollIntoView({ behavior: "smooth"});
   } else if (event.target.classList.contains('blog')) {
-    document.querySelector('#blog').scrollIntoView({ behavior: "smooth", block: "start"});
+    document.querySelector('#blog').scrollIntoView({ behavior: "smooth" });
   }  else if (event.target.classList.contains('team')){
-    document.querySelector('#team').scrollIntoView({ behavior: "smooth", block: "start"});
+    document.querySelector('#team').scrollIntoView({ behavior: "smooth" });
   }
     else if (event.target.classList.contains('contact')) {
-    document.querySelector('#contact').scrollIntoView({ behavior: "smooth", block: "start"});
+    document.querySelector('#contact').scrollIntoView({ behavior: "smooth", block: "start" });
   }
 }
 
