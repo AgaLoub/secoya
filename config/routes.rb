d2fb63a33fc9cfe4bad7265ac67e2c79b6e05296
Agathe Loubert <agaloubert@hotmail.com>
2018-02-26 15:55:52 +0100

    mentions légales done

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   get 'blog', to: 'blog#home', as: :blog
+  get 'legal', to: 'legal#home', as: :legal
 
   resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
