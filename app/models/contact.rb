25c250999c5f4063641d2e8ac551e444c45425f6
Agathe Loubert <agaloubert@hotmail.com>
2018-02-28 15:43:51 +0100

    change

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,7 +7,7 @@ class Contact < MailForm::Base
   def headers
     {
       :subject => "Contact Form",
-      :to => "agaloubert@hotmail.com",
+      :to => "contact@secoya-ecotournage.fr",
       :from => %("#{name}" <#{email}>)
     }
   end
